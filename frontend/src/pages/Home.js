3ea47b1fb19b30194a981c942c5bc298552b163d
Andrew Lloyd <andrew85.lloyd@gmail.com>
2023-07-17 21:32:16 -0700

    Add create instance button

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { Button } from '@mui/material';
 import InstanceTable from '../components/InstanceTable';
 
 const Home = () => {
   return (
     <>
     <h1>Instances</h1>
+    <Button size="small" variant="outlined">+ Create Instance</Button>
     <InstanceTable></InstanceTable>
     <Outlet />
   </>

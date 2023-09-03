8424328d431488fe9f03ff183017ed4c6be4ce45
Andrew Lloyd <andrew85.lloyd@gmail.com>
2023-09-03 15:51:45 -0700

    Fix metric names

--- internal/pkg/datadog/types.go
+++ internal/pkg/datadog/types.go
@@ -3,6 +3,6 @@ package datadog
 type MetricName string
 
 const (
-	MetricNameHerokuLogin  MetricName = "heroku.login"
-	MetricNameHerokuGithub MetricName = "heroku.github"
+	MetricNameHerokuLogin MetricName = "heroku.login"
+	MetricNameGithubLogin MetricName = "github.login"
 )
--- internal/pkg/web/web.go
+++ internal/pkg/web/web.go
@@ -210,7 +210,7 @@ func (s WebServer) tmpHandler(w http.ResponseWriter, req *http.Request) {
 
 func (s WebServer) loginGithub(w http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
-	s.ddClient.Publish(ctx, datadog.MetricNameHerokuGithub, 1)
+	s.ddClient.Publish(ctx, datadog.MetricNameGithubLogin, 1)
 	user, err := github.UserFromContext(ctx)
 	if err != nil {
 		s.errorLogAndRedirect(w, req, fmt.Sprintf("getting user from context: %s", err), "user authentication error")
